7341f2da1f8f66977ffd7171cd496f46b5ae4236
pasillos099 <ma.pasillos@dawsoncollege.qc.ca>
2024-09-20 06:12:51 +0000

    act.2

--- area.py
+++ area.py
@@ -14,3 +14,10 @@ surface_area = 2 * (length * width + length * height + width * height)
 
 # Print the results
 print(f"Your {length} X {width} X {height} cuboid has a volume of {volume} cubic feet and a surface area of {surface_area} square feet.")
+
+
+
+
+price = float(input("Enter the price of the food:"))
+priceTaxIncluded = price * 0.18 + price
+print("This is the price tax included:" , priceTaxIncludedl)
